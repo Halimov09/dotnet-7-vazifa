06c63603f33c4ab149ae22a9b94313a31589b961
halimov09 <sunnatullohhalimov@gmail.com>
2024-08-25 15:22:20 +0500

    first

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// palindrom word 1
+// Console.Write("soz kiriting: ");
+// string str = Console.ReadLine();
+// bool changeStr = false;
+// string reversedString = new string(str.Reverse().ToArray());
+
+// for (int i = 0; i < str.Length; i++)
+// {
+//     if (str[i] == reversedString[i])
+//     {
+//         changeStr = true;
+//     }else{
+//         changeStr = false;
+//     }
+// }
+
+// if (changeStr == true)
+// {
+//     Console.WriteLine($"bu so'z {str} palindrom so'z");
+// }else{
+//     Console.WriteLine($"bu so'z {str} palindrom so'z emas");
+// }
